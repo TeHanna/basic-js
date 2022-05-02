acefa6df4679e0932a0595a30475d881599a5286
Hanna <azotka13@gmail.com>
2022-05-02 17:37:02 +0300

    feat: add solutions to dream-team, file-names, get-email-domains

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  let arrString = members.filter( (item) => typeof item === "string");
+  return arrString.map((item) => item.trim()[0].toUpperCase()).sort().join('');
+
 }
 
 module.exports = {
