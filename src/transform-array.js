f70f12f08c1bf368775aa43b524c57a4b85c4862
Hanna <azotka13@gmail.com>
2022-05-02 17:52:37 +0300

    feat: add solutions sum-digits, transform-array, what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (!Array.isArray(arr))
+  throw new Error("'arr' parameter must be an instance of the Array!");
+
+  let array = arr.map((item) => item);
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "--discard-next") {
+      array[i] = '';
+      array[i + 1] = '';
+    }
+    if (array[i] === "--double-next") {
+      array[i] = array[i + 1];
+    }
+    if (array[i] === "--double-prev") {
+      array[i] = array[i - 1];
+    }
+    if (array[i] === "--discard-prev") {
+      array[i] = '';
+      array[i - 1] = '';
+    }
+  }
+
+  return array.filter((item) => item != '' && item != undefined)
 }
 
 module.exports = {
